ad7fb7ac35357a2f8e6924a377ae2521f5d2f328
Angel Adrian Alvarez Flores <angel.alvarezf@uanl.edu.mx>
2024-03-27 16:52:21 -0600

    Cambios para introducir el login

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def principal_page():
+    return render_template("index.html")
+@app.route("/login")
+def login():
+    return render_template('Form.html')
 if __name__ == "__main__":
-    app.run(debug=True,port=8000)
+    app.run()
