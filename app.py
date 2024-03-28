6e3b76a7537ebcf3a1d0247df74af105578ab093
Angel Adrian Alvarez Flores <angel.alvarezf@uanl.edu.mx>
2024-03-27 20:28:28 -0600

    POST en login

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from modulos import eckeys
 
 app = Flask(__name__)
 
 @app.route("/")
 def principal_page():
     return render_template("index.html")
-@app.route("/login")
+@app.route("/login",  methods=["GET","POST"])
 def login():
-    return render_template('Form.html')
+    if request.method == "POST":
+        print(request.form["usuario"])
+        print(request.form["password"])
+        return render_template('Form.html')
+    else:
+        return render_template('Form.html')
 if __name__ == "__main__":
     app.run()
