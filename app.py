62a0c5b9f81d52593409db088904b7ef6bc2debd
RSKR <raul.riost@uanl.edu.mx>
2024-04-03 19:00:57 -0600

    Update app.py
    - Generar clave para Coockie de sesion.
    - Cierre de sesion.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from modulos import eckeys, users, fernet, rsa, restore
+from os import urandom
 
 app = Flask(__name__)
+
+# Generar clave de sesion
+app.secret_key = urandom(32)
 
 @app.route("/")
 def principal_page():
@@ -13,7 +17,10 @@ def login():
         user = request.form["usuario"]
         passw = request.form["password"]
         users.create_db()
-        users.login(user, passw)
+
+        if users.login(user, passw):
+            session['usuario'] = user
+
         return render_template('Form.html')
     else:
         return render_template('Form.html')
@@ -28,6 +35,14 @@ def registro():
     #eckeys.keys_generator(user,passw)
     #rsa.rsa_key_generator(user, passw)
     return render_template("registro.html")
+
+@app.route('/logout')
+def logout():
+    if 'usuario' in session:
+        session.pop('usuario')
+    
+    return redirect(url_for('principal_page'))
+
 
 if __name__ == "__main__":
     app.run() # host='0.0.0.0'
